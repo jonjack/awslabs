f9aa470b0c37a90aedd0311ddc7c7459723f406b
Jon Jackson <jonathan.x.jackson@gmail.com>
2024-08-26 22:16:26 +0100

    Added base path to serve Github pages on path /awslabs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/awslabs/',
   title: "AWS Labs",
   description: "A random collection of AWS architectures ",
   themeConfig: {
